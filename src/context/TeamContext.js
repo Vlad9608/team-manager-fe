191e02fc63a833522400aa50b98315fc5936d006
Vlad9608 <106959927+Vlad9608@users.noreply.github.com>
2022-07-22 12:34:31 +0300

    using axios instead of fetch

--- src/context/TeamContext.js
+++ src/context/TeamContext.js
@@ -1,58 +1,62 @@
-import React, { createContext, useState, } from 'react';
+import React, { createContext, useState } from "react";
+import axios from "axios";
 
 export const TeamContext = createContext();
 
+const baseUrl = "http://localhost:8000/teams/";
+
 const TeamContextProvider = (props) => {
-  const [teams, setTeams] = useState(null)
+  const [teams, setTeams] = useState(null);
   // TODO: use selectedTeam into teamDetails page
   // TODO: get teamId from useParams to use within the getTeam function
-  const [selectedTeam, setSelectedTeam] = useState(null)
-
- 
+  const [selectedTeam, setSelectedTeam] = useState(null);
 
   const getAllTeams = () => {
-    fetch('http://localhost:8000/teams/')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setTeams(data);
-      })
-  }
+    axios.get(baseUrl).then((response) => {
+      setTeams(response.data);
+    });
+  };
 
   const createTeam = (team) => {
-    fetch('http://localhost:8000/teams/', {
-      method: 'POST',
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(team)
-    }).then(() => {
-      console.log('new team added');
+    axios.post(baseUrl, {
+      // https://www.freecodecamp.org/news/how-to-use-axios-with-react/
+      // to do handleChangeInput react
+    }).then((response) => {
+      setTeams(response.data)
     })
-  }
+  };
 
-  const getTeam = (teamId) => {
+  const getTeam = (teamId) => { 
     fetch(`http://localhost:8000/teams/${teamId}`)
-      .then(res => {
+      .then((res) => {
         return res.json();
       })
-      .then(data => {
+      .then((data) => {
         setSelectedTeam(data);
-      })
-  }
+      });
+  };
 
   const deleteTeam = (teamId) => {
-    fetch('http://localhost:8000/teams/' + teamId, {
-      method: 'DELETE'
-    })
-    .then(data => {
-      getAllTeams()
-    })
-  }
+    fetch("http://localhost:8000/teams/" + teamId, {
+      method: "DELETE",
+    }).then((data) => {
+      getAllTeams();
+    });
+  };
 
   return (
-    <TeamContext.Provider value={{ teams, selectedTeam, getTeam, getAllTeams, createTeam, deleteTeam }}>
+    <TeamContext.Provider
+      value={{
+        teams,
+        selectedTeam,
+        getTeam,
+        getAllTeams,
+        createTeam,
+        deleteTeam,
+      }}
+    >
       {props.children}
     </TeamContext.Provider>
-  )
-}
+  );
+};
 export default TeamContextProvider;
